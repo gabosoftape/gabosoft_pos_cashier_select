be43bdfcf47fdb6bee821bfd4fc2db544c59e94e
gabriel pabon <gabosoft.ape@gmail.com>
2018-12-27 15:07:55 -0500

    test log

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -147,7 +147,8 @@ odoo.define('pos_choosing_cashier', function(require){
             var list = [];
             for (var i = 0; i < this.pos.users.length; i++) {
                 var user = this.pos.users[i];
-                console.log(user);
+                var id = this.pos.id;
+                console.log(id);
                 if (!options.only_managers || user.role === 'manager') {
                     list.push({
                         'label': user.name,
