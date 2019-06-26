e04e8bb1876382df0a153b7cd7af7c022bc4114b
Gabosoft Develop <gabosoft.ape@gmail.com>
2019-06-26 17:00:24 -0500

    init 12

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -115,6 +115,24 @@ odoo.define('pos_choosing_cashier', function(require){
         }
     });
 
+    PopupWidget.include({
+        show: function(options) {
+            if (this.pos.barcode_reader.on_cashier_screen) {
+                if(this.$el) {
+                    this.$el.removeClass('oe_hidden');
+                }
+                if (typeof options === 'string') {
+                    this.options = {title: options};
+                } else {
+                    this.options = options || {};
+                }
+                this.renderElement();
+            } else {
+                this._super(options);
+            }
+        },
+    });
+
     ScreenWidget.include({
         barcode_cashier_action: function(code){
             var users = this.pos.users;
@@ -147,10 +165,7 @@ odoo.define('pos_choosing_cashier', function(require){
             var list = [];
             for (var i = 0; i < this.pos.users.length; i++) {
                 var user = this.pos.users[i];
-                var order = self.pos.get_order();
-
-                console.log(order.cid);
-                if (user.role === "cashier") {
+                if (!options.only_managers || user.role === 'manager') {
                     list.push({
                         'label': user.name,
                         'item':  user,
